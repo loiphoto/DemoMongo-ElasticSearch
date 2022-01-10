ab5ed4e1df0b1c278420d1584008372a81ca30b3
loi <loiphoto19111999@gmail.com>
2022-01-10 13:37:43 +0700

    update

--- src/main/java/com/example/demoelasticsearch/repository/elasticsearch/base/StudentRepository.java
+++ src/main/java/com/example/demoelasticsearch/repository/elasticsearch/base/StudentRepository.java
@@ -1,14 +1,8 @@
 package com.example.demoelasticsearch.repository.elasticsearch.base;
 
 import com.example.demoelasticsearch.entity.Student;
-import com.example.demoelasticsearch.model.response.TotalScoreResponse;
-import org.elasticsearch.action.search.SearchResponse;
-import org.springframework.data.elasticsearch.annotations.Query;
-import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface StudentRepository extends ElasticsearchRepository<Student, String> {
